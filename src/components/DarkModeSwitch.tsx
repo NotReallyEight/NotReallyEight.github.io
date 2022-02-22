6055189dbca62ce496cc18e47aabab74dbaeae92
NotReallyEight <wangzndg@gmail.com>
2022-02-22 23:17:22 +0100

    chore: general cleanup

--- src/components/DarkModeSwitch.tsx
+++ src/components/DarkModeSwitch.tsx
@@ -1,9 +1,15 @@
 import { ChangeEvent, Component } from "react";
 import { Switch } from "@mantine/core";
 
-export default class DarkModeSwitch extends Component {
+type DarkModeSwitchProps = {
+  action(): void;
+}
+
+export default class DarkModeSwitch extends Component<DarkModeSwitchProps> {
   state = {
-    darkModeEnabled: localStorage.getItem("darkmode") === "true",
+    darkModeEnabled:
+      localStorage.getItem("darkmode") === "true" ||
+      localStorage.getItem("darkmode") == null,
   };
   handleChange(event: ChangeEvent<HTMLInputElement>) {
     this.setState({ darkModeEnabled: event.currentTarget.checked });
@@ -15,22 +21,19 @@ export default class DarkModeSwitch extends Component {
 
   render(): JSX.Element {
     return (
-      <>
-        <p>
-          The dark mode is{" "}
-          {this.state.darkModeEnabled ? "enabled" : "not enabled"}
-        </p>
-        <Switch
-          checked={this.state.darkModeEnabled}
-          color="yellow"
-          offLabel={"Off"}
-          onChange={(event) => this.handleChange(event)}
-          onLabel={"On"}
-          label={"Dark Mode"}
-          m={10}
-          size="md"
-        ></Switch>
-      </>
+      <Switch
+        checked={this.state.darkModeEnabled}
+        color="yellow"
+        offLabel={"Off"}
+        onChange={(event) => {
+          this.handleChange(event);
+          this.props.action();
+        }}
+        onLabel={"On"}
+        label={"Dark Mode"}
+        m={10}
+        size="md"
+      ></Switch>
     );
   }
 }
--- src/routers/Home.tsx
+++ src/routers/Home.tsx
@@ -6,10 +6,24 @@ import { Component } from "react";
 import DarkModeSwitch from "../components/DarkModeSwitch";
 
 export default class Home extends Component {
-  render() {
+  state = {
+    darkModeEnabled:
+      localStorage.getItem("darkmode") === "true" ||
+      localStorage.getItem("darkmode") == null,
+  };
+
+  handler = this.handleDarkModeChange.bind(this);
+
+  handleDarkModeChange(): void {
+    this.setState({
+      darkModeEnabled: this.state.darkModeEnabled,
+    });
+  }
+
+  render(): JSX.Element {
     return (
       <div className="Home">
-        <DarkModeSwitch />
+        <DarkModeSwitch action={this.handleDarkModeChange}/>
         <Title title="Hi, I'm NotReallyEight" />
         <HomeIntroduction />
         <FAQSection />
