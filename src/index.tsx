db42992b24423209afbc3e24e76c01df344978a6
Eight∞ <wangzndg@gmail.com>
2022-06-16 22:21:12 +0200

    refactor: migrate from react v17 to v18

--- src/index.tsx
+++ src/index.tsx
@@ -1,26 +1,27 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import "./styles/index.css";
 import Home from "./routers/Home";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import * as ReactDOMClient from "react-dom/client";
 
-ReactDOM.render(
-	<BrowserRouter>
-		<Routes>
-			<Route
-				path="/"
-				element={
-					<React.StrictMode>
-						<meta
-							name="viewport"
-							content="width=device-width, initial-scale=1.0"
-						/>
-						<Home />
-					</React.StrictMode>
-				}
-			/>
-			<Route path="*" element={<div>404</div>} />
-		</Routes>
-	</BrowserRouter>,
-	document.getElementById("root")
+const root = ReactDOMClient.createRoot(document.getElementById("root")!);
+
+root.render(
+  <BrowserRouter>
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <React.StrictMode>
+            <meta
+              name="viewport"
+              content="width=device-width, initial-scale=1.0"
+            />
+            <Home />
+          </React.StrictMode>
+        }
+      />
+      <Route path="*" element={<div>404</div>} />
+    </Routes>
+  </BrowserRouter>
 );
