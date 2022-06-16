fed70e4c27141f714642d64a7802e3fbb95a487f
Eight∞ <wangzndg@gmail.com>
2022-06-16 21:49:03 +0200

    feat: add Footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,12 @@
+import { Component } from "react";
+import "../styles/Footer.css";
+
+export default class Footer extends Component {
+  render(): JSX.Element {
+    return (
+      <div className="Footer">
+        <div className="footer__text">Made with ❤️ by Eight</div>
+      </div>
+    );
+  }
+}
