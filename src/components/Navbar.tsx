3776169d20d19fb70de6f6323b66ec189cb0b058
Eight∞ <wangzndg@gmail.com>
2022-06-17 19:06:05 +0200

    feat: add navigation

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,28 @@
 import { Component } from "react";
 import "../styles/Navbar.css";
 
+type NavbarSection =
+  | "about-me"
+  | "experiences"
+  | "introduction"
+  | "projects"
+  | "skills";
+
 export default class Navbar extends Component {
+  handleNavbarClick(section: NavbarSection): undefined {
+    const home = document.querySelector(".home");
+    const sectionElement = document.querySelector(`.${section}`) as HTMLElement;
+
+    if (!home) return;
+
+    window.history.pushState(null, "", `#${section}`);
+
+    home.scrollTo({
+      top: sectionElement.offsetTop,
+      behavior: "smooth",
+    });
+  }
+
   render(): JSX.Element {
     return (
       <div className="navbar">
@@ -11,7 +32,42 @@ export default class Navbar extends Component {
           alt="Navbar Logo"
           width={100}
           height={100}
+          onClick={() => this.handleNavbarClick("introduction")}
         ></img>
+        <div className="navbar__links">
+          <div
+            className="navbar__link"
+            onClick={() => {
+              this.handleNavbarClick("about-me");
+            }}
+          >
+            About Me
+          </div>
+          <div
+            className="navbar__link"
+            onClick={() => {
+              this.handleNavbarClick("experiences");
+            }}
+          >
+            Experiences
+          </div>
+          <div
+            className="navbar__link"
+            onClick={() => {
+              this.handleNavbarClick("projects");
+            }}
+          >
+            Projects
+          </div>
+          <div
+            className="navbar__link"
+            onClick={() => {
+              this.handleNavbarClick("skills");
+            }}
+          >
+            Skills
+          </div>
+        </div>
       </div>
     );
   }
